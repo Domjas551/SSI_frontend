9a23d13ec49e0cbccddfd9595c5108dad92d5347
Sabuk <sabukaj02@gmail.com>
2024-11-07 15:42:55 +0100

    zmiana stylu przycisków w formularzu

--- src/components/UserEditForm.js
+++ src/components/UserEditForm.js
@@ -1,65 +1,71 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import '../stylesheets/EditForm.css';
 
-function UserEditForm(props){
-
-    const [user, setUser] = useState(props.user)
+function UserEditForm(props) {
+    const [user, setUser] = useState(props.user);
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        updateUsers(event.target.name.value,event.target.surname.value,
-            event.target.position.value,event.target.is_active.value)
+        updateUsers(
+            event.target.name.value,
+            event.target.surname.value,
+            event.target.position.value,
+            event.target.is_active.value
+        );
+    };
 
-    }
-
-    async function updateUsers(name,surname,position,is_active){
-
+    async function updateUsers(name, surname, position, is_active) {
         const requestOptions = {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id: window.location.pathname.substring(21,), name: name, surname:surname,
-            position:position, is_active: is_active})
+            body: JSON.stringify({
+                id: window.location.pathname.substring(21),
+                name: name,
+                surname: surname,
+                position: position,
+                is_active: is_active
+            })
         };
 
-        await fetch("/user",requestOptions)
+        await fetch("/user", requestOptions)
             .then(res => res.json())
             .then((json) => {
-                setUser(json)
-                alert("Edycja danych przebiegła pomyślnie")
+                setUser(json);
+                alert("Edycja danych przebiegła pomyślnie");
             })
-            .catch((error)=>{
-                console.log('Data fetching error.',error)
+            .catch((error) => {
+                console.log('Data fetching error.', error);
             });
     };
 
     return (
-        <main className="main-content">
-            <div className="content-container">
-                <h1 className="heading">Edycja danych</h1>
+        <main className="form_main-content">
+            <div className="form_content-container">
+                <h1 className="form_heading">Edycja danych</h1>
                 <form onSubmit={handleSubmit}>
                     <p>Name</p>
-                    <input name={"name"} type={"text"} placeholder={"Name"} defaultValue={user.name}/>
+                    <input name="name" type="text" placeholder="Name" defaultValue={user.name} />
                     <p>Surname</p>
-                    <input name={"surname"} type={"text"} placeholder={"Surname"} defaultValue={user.surname}/>
+                    <input name="surname" type="text" placeholder="Surname" defaultValue={user.surname} />
                     <p>Position</p>
-                    <select name={"position"} defaultValue={user.position}>
-                        <option value={"pracownik"}>Pracownik</option>
-                        <option value={"kierownik"}>Kierownik</option>
-                        <option value={"administrator"}>Administrator</option>
+                    <select name="position" defaultValue={user.position}>
+                        <option value="pracownik">Pracownik</option>
+                        <option value="kierownik">Kierownik</option>
+                        <option value="administrator">Administrator</option>
                     </select>
                     <p>Is active</p>
-                    <select name={"is_active"} defaultValue={user.is_active}>
+                    <select name="is_active" defaultValue={user.is_active}>
                         <option value={1}>Active</option>
                         <option value={0}>Unactive</option>
                     </select>
-                    <div className="button-container">
-                        <button type="submit" className="b1">Submit</button>
+                    <div className="form_button_container">
+                        <button type="submit" className="form_button">Submit</button>
                     </div>
                 </form>
             </div>
         </main>
-    )
-
+    );
 }
 
 export default UserEditForm;
